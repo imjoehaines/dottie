e18121e62a7d3aec99cf696df585ad7144118898
Joe Haines <hello@joehaines.co.uk>
2020-10-25 14:09:48 +0000

    Extract parsing logic into Parser class

---
+++ lib/dottie/parser.rb
@@ -0,0 +1,31 @@
+module Dottie
+  class Parser
+    def self.parse(path)
+      sections = {}
+
+      File.open(path) do |file|
+        section = nil
+
+        file.each_line do |line|
+          matches = /^--([A-Z]+)--$/.match(line)
+
+          if matches
+            section = matches[1].downcase.to_sym
+
+            raise "Duplicate '--#{matches[1]}--' section found" if sections.has_key?(section)
+
+            sections[section] = ""
+
+            next
+          end
+
+          raise "Must begin a test file with --TEST--" unless section
+
+          sections[section] += line
+        end
+      end
+
+      sections
+    end
+  end
+end
