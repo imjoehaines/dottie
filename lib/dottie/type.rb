77ea35566439153a581f04db6e062101f7cd3033
Joe Haines <hello@joehaines.co.uk>
2020-10-25 10:30:48 +0000

    Cache type instances per file type
    These classes shouldn't have state, so there's no need to create
    new instances every time

--- lib/dottie/type.rb
+++ lib/dottie/type.rb
@@ -5,9 +5,20 @@ module Dottie
   module Type
     def self.for(extension)
       case extension
-        when ".phpt" then Dottie::Type::Php.new
-        when ".rubyt" then Dottie::Type::Ruby.new
+        when ".phpt" then php_type
+        when ".rubyt" then ruby_type
         else raise "Unknown file type '#{extension}'!"
       end
     end
+
+    private
+
+    def self.php_type
+      @@php_type ||= Dottie::Type::Php.new
+    end
+
+    def self.ruby_type
+      @@ruby_type ||= Dottie::Type::Ruby.new
+    end
+  end
 end
