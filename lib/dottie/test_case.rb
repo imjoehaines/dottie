270c48079f8660abc5c654a77ff55d23fffa61fa
Joe Haines <hello@joehaines.co.uk>
2020-10-25 16:06:33 +0000

    Pass Runner to TestCase#run

--- lib/dottie/test_case.rb
+++ lib/dottie/test_case.rb
@@ -6,29 +6,28 @@ module Dottie
     attr_reader :expect
     attr_reader :result
 
-    def initialize(test_type, runner, test:, file:, expect:, skipif: nil)
+    def initialize(test_type, test:, file:, expect:, skipif: nil)
       @test_type = test_type
-      @runner = runner
       @test = test
       @file = file
       @expect = expect
       @skipif = skipif
     end
 
-    def run
-      return Result.new(skipped: true) if should_skip?
+    def run(runner)
+      return Result.new(skipped: true) if should_skip?(runner)
 
-      @result = @runner.run(@test_type.command, @file)
+      @result = runner.run(@test_type.command, @file)
 
       Result.new(success: @expect == @result)
     end
 
     private
 
-    def should_skip?
+    def should_skip?(runner)
       return false if @skipif.nil?
 
-      output = @runner.run(@test_type.command, @skipif)
+      output = runner.run(@test_type.command, @skipif)
 
       output.downcase.start_with?("skip")
     end
