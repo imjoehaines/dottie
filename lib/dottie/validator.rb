758c5a7041b2b04b4f1ad9675b36ebf195f38e4b
Joe Haines <hello@joehaines.co.uk>
2020-10-26 21:21:34 +0000

    Add basic validation of parsed tests

--- lib/dottie/validator.rb
+++ lib/dottie/validator.rb
@@ -1,6 +1,14 @@
 module Dottie
   class Validator
     def validate(sections)
+      raise "Test files must have a 'TEST' section" unless sections[:test]
+      raise "Test files must have a 'FILE' section" unless sections[:file]
+
+      unless sections[:expect] || sections[:expectf]
+        raise "Test files must have either an 'EXPECT' or 'EXPECTF' section"
+      end
+
+      true
     end
   end
 end
