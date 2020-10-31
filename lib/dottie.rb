b2c800eb2b145427864532ae50f495ba72b0d93d
Joe Haines <hello@joehaines.co.uk>
2020-10-31 12:12:58 +0000

    Add basic option parsing
    This needs to be factored out somewhere, but it's a start!

--- lib/dottie.rb
+++ lib/dottie.rb
@@ -1,3 +1,13 @@
 module Dottie
   VERSION = "0.0.1".freeze
+
+  class << self
+    def banner(error: false)
+      if error
+        "❌ Dottie #{Dottie::VERSION} :("
+      else
+        "✨ Dottie #{Dottie::VERSION} :)"
+      end
+    end
+  end
 end
