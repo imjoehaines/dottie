4d78ff3e7915369cfd65225f61fe599294582156
Joe Haines <hello@joehaines.co.uk>
2020-10-31 10:54:07 +0000

    Refactor to allow multiple formatters

--- lib/dottie/formatter.rb
+++ lib/dottie/formatter.rb
@@ -1,62 +1,12 @@
-require_relative "colour"
+require_relative "formatter/pretty"
 
-module Dottie
-  class Formatter
-    def test_result(test_case, result)
-      if result.success?
-        "#{colour("✔").green} #{test_case.test}"
-      elsif result.skipped?
-        "#{colour("-").cyan} #{test_case.test}"
-      else
-        <<~TEXT
-          #{colour("✖").red} #{test_case.test}
-          #{colour("Expected output:").bold}
-          #{test_case.expected}
-          #{colour("Actual output:").bold}
-          #{test_case.result}
-        TEXT
+module Dottie::Formatter
+  class << self
+    def for(type)
+      case type
+      when :pretty then Dottie::Formatter::Pretty.new
+      else raise "Unknown formatter type '#{type}'!"
       end
     end
-
-    def suite_result(results)
-      total = results.count
-      skips = results.count(&:skipped?)
-      failures = results.count(&:failed?)
-      plural = ->(count) { count == 1 ? "test" : "tests" }
-
-      output = "\n#{colour("Ran #{total} #{plural.(total)}!").bold}\n"
-
-      if skips > 0
-        output << colour("#{skips} skipped #{plural.(skips)}").cyan.to_s << "\n"
-      end
-
-      if failures > 0
-        output << colour("#{failures} failed #{plural.(failures)}").red.to_s << "\n"
-      end
-
-      output << "\n" << success_or_fail(success: failures == 0)
-
-      output
-    end
-
-    def no_tests_found(directory)
-      <<~TEXT
-        No tests found in '#{directory}'
-
-        #{success_or_fail(success: false)}
-      TEXT
-    end
-
-    private
-
-    def success_or_fail(success:)
-      if success
-        colour("SUCCESS").green.bold.to_s
-      else
-        colour("FAIL").red.bold.to_s
-      end
-    end
-
-    def colour(*args); Dottie::Colour.new(*args) end
   end
 end
