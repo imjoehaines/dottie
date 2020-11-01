d9fba051cc717a8672377f109aa33ca199216ba9
Joe Haines <hello@joehaines.co.uk>
2020-11-01 13:13:49 +0000

    Make the thread pool an actual thread pool

--- lib/dottie/thread_pool.rb
+++ lib/dottie/thread_pool.rb
@@ -1,14 +1,28 @@
 module Dottie
   class ThreadPool
-    def initialize
-      @threads = []
+    STOP = Object.new
+    private_constant :STOP
+
+    def initialize(max_threads:)
+      @queue = Queue.new
+
+      @threads = max_threads.times.map do
+        Thread.new do
+          while job = @queue.pop
+            break if job == STOP
+
+            job.call
+          end
+        end
+      end
     end
 
     def execute(&block)
-      @threads << Thread.new(&block)
+      @queue << block
     end
 
     def finish!
+      @threads.each { @queue << STOP }
       @threads.each(&:join)
     end
   end
