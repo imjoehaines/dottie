fa643289fa61fd02964e5d38a9898bc55ec3e806
Joe Haines <hello@joehaines.co.uk>
2022-11-10 20:33:06 +0000

    Include test directory name with pretty formatter

--- lib/dottie/formatter/pretty.rb
+++ lib/dottie/formatter/pretty.rb
@@ -3,7 +3,9 @@ require_relative "simple"
 module Dottie::Formatter
   class Pretty < Simple
     def test_result(result)
-      "#{super(result)} #{result.test_name.chomp}\n"
+      directory = colour(result.directory).dim
+
+      "#{super(result)} #{directory} #{result.test_name.chomp}\n"
     end
   end
 end
