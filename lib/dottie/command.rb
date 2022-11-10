ed4b6bfcc761ca9e8f5e357564b30704118786c6
Joe Haines <hello@joehaines.co.uk>
2022-11-10 20:56:12 +0000

    Support unknown commands by guessing
    It's _likely_ that a command will be valid by simply removing the
    leading '.' and trailing 't' on a file, assuming the file follows the
    dot T convention
    
    This won't always be the case, e.g. Python uses '.py' so '.pyt' would be
    natural but 'py' isn't (usually!) the Python interpreter
    
    This doesn't completely replace registering commands — if you need to
    run docker, supply command line arguments or environment variables,
    etc... then you'll still need to register a custom command

--- lib/dottie/command.rb
+++ lib/dottie/command.rb
@@ -1,14 +1,17 @@
 module Dottie::Command
   class << self
     COMMANDS = {
-      ".phpt" => "php",
-      ".rubyt" => "ruby",
+      ".rbt" => "ruby",
     }
 
     def for(extension)
-      raise "Unknown file type '#{extension}'!" unless COMMANDS.key?(extension)
-
-      COMMANDS[extension]
+      if COMMANDS.key?(extension)
+        COMMANDS[extension]
+      else
+        # if we don't know what the command is, we guess by removing a leading
+        # "." and trailing "t", e.g. '.rubyt' => 'ruby', '.phpt' => 'php'
+        register(extension, extension.delete_prefix(".").delete_suffix("t"))
+      end
     end
 
     def register(extension, command)
